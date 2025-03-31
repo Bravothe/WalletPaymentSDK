5968cf218e9cafa4062ecab76a19d2986a0dd72b
Bravothe <Bravo@evzone.app>
2025-03-31 09:29:00 +0300

    Initial Commit

---
+++ WalletPaymentSDK/WalletPaymentSDK.h
@@ -0,0 +1,18 @@
+//
+//  WalletPaymentSDK.h
+//  WalletPaymentSDK
+//
+//  Created by Bravo on 31/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WalletPaymentSDK.
+FOUNDATION_EXPORT double WalletPaymentSDKVersionNumber;
+
+//! Project version string for WalletPaymentSDK.
+FOUNDATION_EXPORT const unsigned char WalletPaymentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalletPaymentSDK/PublicHeader.h>
+
+
